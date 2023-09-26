3697a910a28e92e06ddca7bd3c046e644c41c0e7
caioaquino <caioaquino.profissional@gmail.com>
2023-09-26 12:57:45 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import {
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Layout, Menu, theme, Breadcrumb } from "antd";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          // console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          // console.log(collapsed, type);
+        }}
+        style={{ height: "100vh" }}
+      >
+        <div className="demo-logo-vertical">Home</div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          items={[
+            UserOutlined,
+            VideoCameraOutlined,
+            UploadOutlined,
+            UserOutlined,
+          ].map((icon, index) => ({
+            key: String(index + 1),
+            icon: React.createElement(icon),
+            label: `nav ${index + 1}`,
+          }))}
+        />
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 16,
+            background: colorBgContainer,
+            alignItems: "center",
+            display: "flex",
+          }}
+        >
+          <Breadcrumb
+            items={[{ title: "Caio Nogueira" }, { title: "Início" }]}
+          />
+        </Header>
+        <Content style={{ margin: 24 }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+            }}
+          >
+            content
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default App;
