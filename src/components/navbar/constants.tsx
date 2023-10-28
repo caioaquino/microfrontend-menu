892c9a8e99e10bcefefedf9bb4441b7c4f83d4a5
caioaquino <caioaquino.profissional@gmail.com>
2023-10-28 12:43:14 -0300

    adicionando rota para home

--- src/components/navbar/constants.tsx
+++ src/components/navbar/constants.tsx
@@ -1,14 +1,24 @@
-import { AppstoreAddOutlined, DashboardOutlined } from "@ant-design/icons";
+import {
+  AppstoreAddOutlined,
+  DashboardOutlined,
+  HomeOutlined,
+} from "@ant-design/icons";
 
 export const routes = [
   {
     key: "0",
+    label: "Home",
+    pathname: "",
+    icon: <HomeOutlined />,
+  },
+  {
+    key: "1",
     label: "Dashboard",
     pathname: "dashboard",
     icon: <DashboardOutlined />,
   },
   {
-    key: "1",
+    key: "2",
     label: "Registro",
     pathname: "register",
     icon: <AppstoreAddOutlined />,
